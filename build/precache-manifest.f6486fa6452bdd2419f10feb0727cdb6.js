9d5ceb89b9e11ecbc4e6a8bf0ee64efef9dc455a
pugazhkovan83 <pugazhkovan83@gmail.com>
2020-07-20 00:14:21 +0530

    build1

---
+++ build/precache-manifest.f6486fa6452bdd2419f10feb0727cdb6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1ef7cc0ca4c5fc3924f4ec7d8a03773",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4dcb5e40cb80310942ea",
+    "url": "./static/css/main.2da14e71.chunk.css"
+  },
+  {
+    "revision": "b06efbd6e42f35bf10bb",
+    "url": "./static/js/2.1dd794ee.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.1dd794ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4dcb5e40cb80310942ea",
+    "url": "./static/js/main.6b838a54.chunk.js"
+  },
+  {
+    "revision": "bc79d1d450bd81c5656b",
+    "url": "./static/js/runtime-main.05844e11.js"
+  },
+  {
+    "revision": "3d0bad804a398c51f1d9d0dac3d9a86e",
+    "url": "./static/media/tshirt.3d0bad80.jpg"
+  }
+]);
